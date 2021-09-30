0066dca5e709f413660a2c99df6e06ca21d0475a
karimmakram <karimomakram11@gmail.com>
2021-09-30 16:40:17 +0200

    prepare deployment

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,17 +1,16 @@
 import mongoose from 'mongoose'
 export const connectDb = () => {
-  try {
-    mongoose
-      .connect(`${process.env.local_db_url}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-      })
-      .then(() => {
-        console.log('Db connected')
-      })
-  } catch (error) {
-    process.exit(1)
-  }
+  mongoose
+    .connect(`${process.env.local_db_url}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+    .then(() => {
+      console.log('Db connected')
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
